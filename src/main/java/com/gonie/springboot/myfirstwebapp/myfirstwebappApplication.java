8809f6721d49c5596f497d240c335a044aa469ce
BakariGonie <abdillahbakari93@gmail.com>
2023-12-11 19:22:54 +0300

    first commit

---
+++ src/main/java/com/gonie/springboot/myfirstwebapp/myfirstwebappApplication.java
@@ -0,0 +1,13 @@
+package com.gonie.springboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myfirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(myfirstwebappApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'Akram', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'Akram', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'Akram', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'Akram', 'Learn DevOps', CURRENT_DATE(), false);
